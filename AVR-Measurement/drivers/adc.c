024ae21b054c30d4be5ff358f1b9e95642b0972b
Eirik Haustveit <eirik@la2kta.net>
2013-07-30 13:25:31 +0200

    Initial.

---
+++ AVR-Measurement/drivers/adc.c
@@ -0,0 +1,44 @@
+/*!
+ * \file adc.c
+ *
+ * \author eirik
+ * \date
+ *
+ * [your comment here]
+ */
+
+/*
+This file is part of AVR-Measurement.
+
+    AVR-Measurement is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    AVR-Measurement is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with AVR-Measurement.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+
+/*!
+ * \brief [brief description]
+ *
+ * [detailed description]
+ *
+ * \param[in] [name of input parameter] [its description]
+ * \param[out] [name of output parameter] [its description]
+ * \return [information about return value]
+ * \sa [see also section]
+ * \note [any note about the function you might have]
+ * \warning [any warning if necessary]
+ */
+int main(int argc, char *argv[])
+{
+	return 0;
+}
